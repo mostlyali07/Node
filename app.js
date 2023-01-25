71693f11fe5c51f7b36ac182e50a7f41eb091bda
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-01-26 01:34:17 +0500

    Update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.listen(5000, () => {
+  console.log("Hello Server Started");
+});
 
-
-app.listen(5000, ()=>{
-    console.log("Hello Server Started");
-})
+app.post("/post", async (req, res) => {
+  console.log(req.body);
+});
